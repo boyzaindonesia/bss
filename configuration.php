d72769e7cb94511ac85c48e8bf27a7e270701a34
FebriR <fheby.kere.hore@gmail.com>
2020-10-15 19:08:30 +0700

    Update configuration.php

--- configuration.php
+++ configuration.php
@@ -6,12 +6,6 @@ $cfg['db']['password'] = "";
 $cfg['db']['database'] = "butiksasha";
 $cfg['db']['dbdriver'] = "mysql";
 
-// $cfg['db']['hostname'] = "localhost";
-// $cfg['db']['username'] = "k9188880_butiksa";
-// $cfg['db']['password'] = "na5JMQ$2R%L(";
-// $cfg['db']['database'] = "k9188880_butiksasha";
-// $cfg['db']['dbdriver'] = "mysql";
-
 /* myconfig */
 $config['mt_app_name'] 	= "app";
 $config['mt_app_version'] 	= "0.1";
